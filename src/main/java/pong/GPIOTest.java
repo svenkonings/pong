bc98b438631af2c8859374c60a77f0c6d6bdbed9
meteoorkip <svenkonings@live.nl>
2015-10-16 16:22:06 +0200

    GPIO test

--- src/main/java/pong/GPIOTest.java
+++ src/main/java/pong/GPIOTest.java
@@ -1,28 +1,47 @@
 package pong;
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.*;
 
 public class GPIOTest {
     private static final int OUTPUT_PINS = 17;
 
     public static void main(String[] args) {
         GpioController gpio = GpioFactory.getInstance();
-        GpioPinDigitalOutput[] outputs = new GpioPinDigitalOutput[OUTPUT_PINS];
+        GpioPinDigitalOutput[] pins = new GpioPinDigitalOutput[OUTPUT_PINS];
         for (int i = 0; i < OUTPUT_PINS; i++) {
-            outputs[i] = gpio.provisionDigitalOutputPin(RaspiPin.getPinByName("GPIO " + i));
+            pins[i] = gpio.provisionDigitalOutputPin(RaspiPin.getPinByName("GPIO " + i));
+            pins[i].setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
         }
-        while (true) {
-            for (GpioPinDigitalOutput output : outputs) {
-                output.setState(output.getState().isLow());
-            }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
+        switch (args.length) {
+            case 0:
+                for (GpioPinDigitalOutput pin : pins) {
+                    setState(pin, true, 1000);
+                    setState(pin, false, 1000);
+                }
+                break;
+            case 1:
+                setState(pins[Integer.parseInt(args[0])], true, 60000);
+                break;
+            case 2:
+                setState(pins[Integer.parseInt(args[0])], Boolean.parseBoolean(args[1]), 60000);
+                break;
+            default:
+                System.out.println("Usage: [PIN NUMBER [STATE]]");
+                break;
+        }
+        gpio.shutdown();
+        gpio.unprovisionPin(pins);
+        System.out.println("Pins shutdown");
+    }
+
+    private static void setState(GpioPinDigitalOutput pin, boolean state, long sleep) {
+        pin.setState(state);
+        System.out.println(pin.getName() + " state: " + state + " timeout: " + sleep);
+        try {
+            Thread.sleep(sleep);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 }
